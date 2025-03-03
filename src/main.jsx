b84908e3117c1fdef6a7e6b5be927217607599e2
sorat gessakorn <sorat.g@tcc-technology.com>
2025-03-03 21:41:34 +0700

    add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import App from "./App.jsx";
+import NewPost from "./components/NewPost.jsx";
+
+const router = createBrowserRouter([
+    {path: '/', element: <App/>},
+    {path: '/create-post', element: <NewPost/>},
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router}/>
+    </StrictMode>
 )
