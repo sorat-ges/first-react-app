7a64b287edb5be1999804c5d200f98fa1dc93327
sorat gessakorn <sorat.g@tcc-technology.com>
2025-03-18 21:35:57 +0700

    use action in react dom

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,16 +4,6 @@ import {useLoaderData} from 'react-router-dom';
 
 function PostList() {
     const posts = useLoaderData();
-    function addPostHandler(postData) {
-        fetch("http://localhost:8080/posts/", {
-            method: "POST",
-            body: JSON.stringify(postData),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-        setPosts((existingPosts) => [postData, ...existingPosts]);
-    }
 
     return (
         <>
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,19 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Posts , {loader as postsLoader } from "./routes/Posts.jsx";
-import NewPost from "./routes/NewPost.jsx";
+import Posts, {loader as postsLoader} from "./routes/Posts.jsx";
+import NewPost ,{action as newPostAction }from "./routes/NewPost.jsx";
 import RootLayout from './routes/RootLayout';
 
 const router = createBrowserRouter([
     {
-        path: '', element: <RootLayout/>,
+        path: '/', element: <RootLayout/>,
         children: [
             {
                 path: '/', element: <Posts/>, loader: postsLoader,
-                children: [{path: '/create-post', element: <NewPost/>}]
+                children: [{
+                    path: '/create-post', element: <NewPost/>, action: newPostAction
+                }]
             },
 
         ]
